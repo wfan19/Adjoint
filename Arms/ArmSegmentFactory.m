54d956fa0b6a0e9e49f71b56d8128e61d0fb1355
Bill Fan <fanbenlong@gmail.com>
2023-07-26 15:29:38 -0400

    Added 2d arm segment factory

---
+++ Arms/ArmSegmentFactory.m
@@ -0,0 +1,67 @@
+classdef ArmSegmentFactory
+    %SEGMENTFACTORY Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+
+    methods(Static)
+        function arm_segment = make_2d_2muscle(rho, l_0)
+            g_o_A = Pose2.hat([0, -rho, 0]);
+            g_o_B = Pose2.hat([0, rho, 0]);
+            g_o_rods = {g_o_A; g_o_B};
+
+            g_0_o = Pose2.hat([0, 0, -pi/2]);
+    
+            arm_segment = ArmSegment(Pose2, g_0_o, g_o_rods, l_0);
+            
+            arm_segment.rod_o.mechanics.l_0 = l_0; % Default length of the whole segment
+            
+            arm_segment.rods(1).mechanics = GinaMuscleMechanics(l_0);
+            arm_segment.rods(2).mechanics = GinaMuscleMechanics(l_0);
+        end
+
+        function arm_segment = make_2d_antagonism(rho_inner, rho_outer, l_0)
+            g_o_X = Pose2.hat([0, -rho_outer, 0]);
+            g_o_A = Pose2.hat([0, -rho_inner, 0]);
+            g_o_B = Pose2.hat([0, rho_inner, 0]);
+            g_o_Y = Pose2.hat([0, rho_outer, 0]);
+            obj.g_o_rods = {g_o_X; g_o_A; g_o_B; g_o_Y};
+
+            g_0_o = Pose2.hat([0, 0, -pi/2]);
+    
+            arm_segment = ArmSegment(Pose2, g_0_o, g_o_rods, l_0);
+            
+            arm_segment.rod_o.mechanics.l_0 = l_0; % Default length of the whole segment
+            
+            arm_segment.rods(1).mechanics = BasicPolyBellowMechanics(l_0);
+            arm_segment.rods(2).mechanics = GinaMuscleMechanics(l_0);
+            arm_segment.rods(3).mechanics = GinaMuscleMechanics(l_0);
+            arm_segment.rods(4).mechanics = BasicPolyBellowMechanics(l_0);
+        end
+
+        function arm_segment = make_3d_circular(rho, l_0)
+            % Create list of angles around the circle that the rods will sit at
+            theta = linspace(0, 2*pi, N_rods + 1);
+            theta = theta(1:end-1);
+            
+            g_o_rods = cell(1, N_rods);
+            for i = 1 : n_muscles
+                % Posn of each rod is based on the angle around the circle
+                t_muscle_i = rho * [0; cos(theta(i)); sin(theta(i))];
+
+                % Rotate each rod such that (Y? Z?) axis is tangent to circle
+                R_muscle_i = eul2rotm([yaw_muscles(i), 0, 0], 'xyz');
+                g_o_rods{i} = Pose3.hat(R_muscle_i, t_muscle_i);
+            end
+
+            g_0_o = Pose3(eul2rotm([0, -pi/2, 0], "xyz"), [0; 0; 0]);
+            arm_segment = ArmSegment(Pose3, g_0_o, g_o_rods, l_0);
+            
+            for i = 1 : length(arm_segment.rods)
+                arm_segment.rods(i).mechanics = GinaMuscleMechanics(l_0);
+            end
+        end
+    end
+end
+
--- tests/test_arm_segment_2d_2muscle.m
+++ tests/test_arm_segment_2d_2muscle.m
@@ -1,43 +1,41 @@
-function tests = test_arm_segment
+function tests = test_arm_segment_2d_2muscle
     tests = functiontests(localfunctions);
 end
 
 function setup(testCase)
-    % Build a simple 2-muscle 2D arm
-    l_0 = 0.5;
+
     rho = 0.0254;
-    g_o_A = Pose2.hat([0, rho, 0]);
-    g_o_B = Pose2.hat([0, -rho, 0]);
-    g_o_rods = {g_o_A; g_o_B};
-    
-    g_0_o = Pose2.hat([0, 0, -pi/2]);
+    l_0 = 0.5;
+    arm_segment = ArmSegmentFactory.make_2d_2muscle(rho, l_0);
+
+    % Define all rods to be bellows
+    % TODO: There has to be a better way to do this
+    for i = 1 : length(arm_segment.rods)
+        arm_segment.rods(i).mechanics = BasicBellowMechanics(l_0);
+    end
 
     testCase.TestData.l_0 = l_0; % Default length
-    testCase.TestData.rho = 1 * 0.0254; % Define inter-muscle geometry
-    testCase.TestData.arm_2d = ArmSegment(Pose2, g_0_o, g_o_rods, l_0);
-
-    for i = 1 : length(testCase.TestData.arm_2d.rods)
-        testCase.TestData.arm_2d.rods(i).mechanics = BasicBellowMechanics(l_0);
-    end
+    testCase.TestData.rho = rho; % Define inter-muscle geometry
+    testCase.TestData.arm_segment = arm_segment;
 end
 
 function teardown(testCase)
-    delete(testCase.TestData.arm_2d);
+    delete(testCase.TestData.arm_segment);
 end
 
 %% Construction and copying
 function test_construction_2d(testCase)
-    arm_2d = testCase.TestData.arm_2d;
-    verifyEqual(testCase, testCase.TestData.l_0, arm_2d.rod_o.l);
+    arm_segment = testCase.TestData.arm_segment;
+    verifyEqual(testCase, testCase.TestData.l_0, arm_segment.rod_o.l);
 end
 
 function test_copy(testCase)
     % Create a copy arm and change one of its properties
-    arm_copy = copy(testCase.TestData.arm_2d);
+    arm_copy = copy(testCase.TestData.arm_segment);
     arm_copy.rod_o.l = 2;
 
     % Verify that the property is unchanged on the original arm
-    verifyEqual(testCase, testCase.TestData.arm_2d.rod_o.l, testCase.TestData.l_0);
+    verifyEqual(testCase, testCase.TestData.arm_segment.rod_o.l, testCase.TestData.l_0);
 end
 
 %% Kinematics
@@ -50,7 +48,7 @@ end
 % Test shifting the arm base-curve starting poses to see if they shift 
 % the individual ones as well  
 function test_set_base_pose(testCase)
-    arm = testCase.TestData.arm_2d;
+    arm = testCase.TestData.arm_segment;
     
     arm.g_0_o = Pose2.hat([2, 0, -pi/2]);
 
@@ -59,27 +57,27 @@ function test_set_base_pose(testCase)
 end
 
 function test_get_tip_pose(testCase)
-    tip_pose = testCase.TestData.arm_2d.get_tip_pose();
+    tip_pose = testCase.TestData.arm_segment.get_tip_pose();
     verifyEqual(testCase, Pose2.vee(tip_pose), [0; -0.5; -pi/2]);
 end
 
 %% Mechanics
 % Test retrieving the strains of each muscle, based on the base-curve
 function test_get_strains(testCase)
-    arm = testCase.TestData.arm_2d;
+    arm = testCase.TestData.arm_segment;
     g_circ_right = [1; 0; 2];
     strains = arm.get_strains(g_circ_right);
 
     % I don't have the exact values, so we'll just make sure it's curving
     % in the right direction.
-    verifyLessThan(testCase, strains(1), strains(2))
+    verifyLessThan(testCase, strains(2), strains(1))
 end
 
 % Test computing the forces of each muscle, given an actuation
 % "What are the forces when the actuators are inflated, but the manipulator
 % is held straight?"
 function test_get_forces(testCase)
-    arm = testCase.TestData.arm_2d;
+    arm = testCase.TestData.arm_segment;
     l_0 = testCase.TestData.l_0;
 
     pressures = [0; 20]; % kPa
